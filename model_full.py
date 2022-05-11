3ad017ccf14f96aeb9aefb66bd6512afcc0ee5c2
Olaf98 <100184344+Olaf98@users.noreply.github.com>
2022-05-12 02:51:11 +0400

    Update model_full.py

--- model_full.py
+++ model_full.py
@@ -11,22 +11,68 @@ Original file is located at
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import init
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, width_multiplier=1, trilinear=True, use_ds_conv=False):
+        """A simple 3D Unet, adapted from a 2D Unet from https://github.com/milesial/Pytorch-UNet/tree/master/unet
+        Arguments:
+          n_channels = number of input channels; 3 for RGB, 1 for grayscale input
+          n_classes = number of output channels/classes
+          width_multiplier = how much 'wider' your UNet should be compared with a standard UNet
+                  default is 1;, meaning 32 -> 64 -> 128 -> 256 -> 512 -> 256 -> 128 -> 64 -> 32
+                  higher values increase the number of kernels pay layer, by that factor
+          trilinear = use trilinear interpolation to upsample; if false, 3D convtranspose layers will be used instead
+          use_ds_conv = if True, we use depthwise-separable convolutional layers. in my experience, this is of little help. This
+                  appears to be because with 3D data, the vast vast majority of GPU RAM is the input data/labels, not the params, so little
+                  VRAM is saved by using ds_conv, and yet performance suffers."""
+        super(UNet, self).__init__()
+        _channels = (32, 64, 128, 256, 512)
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.channels = [int(c*width_multiplier) for c in _channels]
+        self.trilinear = trilinear
+        self.convtype = DepthwiseSeparableConv3d if use_ds_conv else nn.Conv3d
+
+        self.inc = DoubleConv(n_channels, self.channels[0], conv_type=self.convtype)
+        self.down1 = Down(self.channels[0], self.channels[1], conv_type=self.convtype)
+        self.down2 = Down(self.channels[1], self.channels[2], conv_type=self.convtype)
+        self.down3 = Down(self.channels[2], self.channels[3], conv_type=self.convtype)
+        factor = 2 if trilinear else 1
+        self.down4 = Down(self.channels[3], self.channels[4] // factor, conv_type=self.convtype)
+        self.up1 = Up(self.channels[4], self.channels[3] // factor, trilinear)
+        self.up2 = Up(self.channels[3], self.channels[2] // factor, trilinear)
+        self.up3 = Up(self.channels[2], self.channels[1] // factor, trilinear)
+        self.up4 = Up(self.channels[1], self.channels[0], trilinear)
+        self.outc = OutConv(self.channels[0], n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
 
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels, conv_type=nn.Conv3d, mid_channels=None):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
+            conv_type(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            conv_type(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -37,11 +83,11 @@ class DoubleConv(nn.Module):
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, conv_type=nn.Conv3d):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
+            nn.MaxPool3d(2),
+            DoubleConv(in_channels, out_channels, conv_type=conv_type)
         )
 
     def forward(self, x):
@@ -51,16 +97,17 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, trilinear=True):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        # if trilinear, use the normal convolutions to reduce the number of channels
+        if trilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, mid_channels=in_channels // 2)
         else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
+
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -80,51 +127,19 @@ class Up(nn.Module):
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
         return self.conv(x)
-    
-    
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
-        super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+
+class DepthwiseSeparableConv3d(nn.Module):
+    def __init__(self, nin, nout, kernel_size, padding, kernels_per_layer=1):
+        super(DepthwiseSeparableConv3d, self).__init__()
+        self.depthwise = nn.Conv3d(nin, nin * kernels_per_layer, kernel_size=kernel_size, padding=padding, groups=nin)
+        self.pointwise = nn.Conv3d(nin * kernels_per_layer, nout, kernel_size=1)
 
     def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
-
-
-
-###################################################################################
-
-
-    
-
-        
-
-###########################################################################################
+        out = self.depthwise(x)
+        out = self.pointwise(out)
+        return out

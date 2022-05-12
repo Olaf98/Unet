b90f2e94c0b13c52213ce7bc4f71e8f84f18fc24
Olaf98 <100184344+Olaf98@users.noreply.github.com>
2022-05-12 14:45:04 +0400

    Update model_full.py

--- model_full.py
+++ model_full.py
@@ -114,12 +114,13 @@ class Up(nn.Module):
         # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
+        if x1.size()[2:] != x2.size()[2:]:
+            x1 = F.interpolate(x1,
+                               x2.size()[2:],
+                               mode='trilinear',
+                               align_corners=False)
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
